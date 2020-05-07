1ef41119babdb6f710560dd66bc5d4ca092b30ca
alexClifton <oooalexoooo@gmail.com>
2020-05-06 18:06:29 -0700

    initial commit

---
+++ src/components/MenuBar/MenuBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-bar.css';
+
+const MenuBar = () => {
+  return (
+    <div className="menu-bar">
+      <ul className="menu-bar-list">
+        <li>Home</li>
+        <li>About</li>
+        <li>MyProfile</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MenuBar;
